7ba6f06244d82b9065ce1ca28279cbe06d7b53ce
jonakls <asmot54@hotmail.com>
2023-05-27 17:56:20 -0500

    feat(service): implement services in initial process of plugin

--- plugin/src/main/java/team/devblook/template/PluginTemplate.java
+++ plugin/src/main/java/team/devblook/template/PluginTemplate.java
@@ -1,17 +1,29 @@
 package team.devblook.template;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import team.devblook.template.service.ServiceManager;
 
 public class PluginTemplate extends JavaPlugin {
 
+    private ServiceManager serviceManager;
+
+    @Override
+    public void onLoad() {
+        this.serviceManager = new ServiceManager();
+    }
+
     @Override
     public void onEnable() {
-        getLogger().info("Plugin enabled");
+        if (this.serviceManager != null) {
+            this.serviceManager.start();
+        }
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Plugin disabled");
+        if (this.serviceManager != null) {
+            this.serviceManager.stop();
+        }
     }
 
 }
